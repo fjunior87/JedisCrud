3a508501debdde1fc10ffa93c90fae9ea07eb828
fjunior87 <fjunior87@gmail.com>
2013-08-31 12:10:10 -0300

    Changed to use @Singleton

--- src/com/xicojunior/jediscrud/factory/JedisPoolFactory.java
+++ src/com/xicojunior/jediscrud/factory/JedisPoolFactory.java
@@ -1,6 +1,5 @@
 package com.xicojunior.jediscrud.factory;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
@@ -8,18 +7,20 @@ import javax.inject.Singleton;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-@ApplicationScoped
+
 public class JedisPoolFactory {
 
 	private String host = "localhost";
 	private JedisPool jedisPool;
-	
+	@Singleton
 	public @Produces JedisPool getJedisPool(){
 		jedisPool = new JedisPool(new JedisPoolConfig(), host);
+		System.out.println("Jedis Pool: " + jedisPool);
 		return jedisPool;
 	}
 	
 	public void detroy(@Disposes JedisPool jedisPool){
+		System.out.println("Destroying :" + jedisPool);
 		jedisPool.destroy();
 	}
 }
